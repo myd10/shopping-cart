653e6d67d20573839ade41a9f694738b2b4ad1f7
myd10 <myd10@georgetown.edu>
2020-04-13 18:10:21 -0400

    Created 'app', fixed Datetime, Dashed Line

--- app/shopping_cart.py
+++ app/shopping_cart.py
@@ -24,12 +24,16 @@ products = [
 ] # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
 
 
-#GOAL 1: fix USD formatting
 #GOAL 2: fix date and time function, display friendly human message without so much code
 #GOAL 3: write data to CSV?
 #Goal 4: get rid of reptition
 #GOAL 5: testing
 #GOAL 6: hit all checkpoints on product outline. 
+
+#
+# MODULES
+#
+from datetime import datetime
 
 
 def to_usd(my_price):
@@ -42,6 +46,41 @@ def to_usd(my_price):
     """
     return f"${my_price:,.2f}" #> $12,000.71
 
+def human_friendly_timestamp(now):
+    """
+    Converts datetime information into a human friendly string
+    Param: now
+    Example:
+        current_year = 2020
+        current_month = 4
+        current_day = 13
+        current_hour = 17
+        current_minute = 25
+        current_second = 12
+    Returns: 2020-4-13 5:25pm
+    """
+    return datetime.now().strftime("%m/%d/%Y %I:%m %p")
+
+def dashed_line():
+    """
+    Prints a dashed line to improve receipt readability
+    Param: none
+    Example: dashed_line
+    Returns: "---------------------"
+    """
+    print("--------------------")
+
+def find_product():
+    """
+    Returns the correct product in a list that matches a given identifier number
+    Param: product identifier (integer)
+    Example: find_product(1)
+    Returns: product[0]["name"]
+    """
+
+
+
+
 #
 # Input information / ID
 #
@@ -53,21 +92,11 @@ total_price = 0
 scanned_ids = [] #this will be a list to store scanned_id's and print after all id's have been entered
 grocery_list = []
 
-#date and time, help from https://www.youtube.com/watch?v=WB5eMfnBI-8
-from datetime import datetime
-now = datetime.now()
-
-current_time = now.strftime("%H:%M:%S") #help from https://www.programiz.com/python-programming/datetime/current-time
-current_year = now.year
-current_month = now.month
-current_day = now.day
-current_hour = now.hour
-current_minute = now.minute
 
 #loop and control over ids accepted
 while True:
-    scanned_id = input("Please enter a product ID number, or DONE: ") #input returns a string datatype
-    if scanned_id == "DONE":
+    scanned_id = input("Please enter a product ID number, or DONE: ")
+    if scanned_id.upper() == "DONE":
         break
     elif int(scanned_id) > 20 or int(scanned_id) < 1:
         print("This ID is not registered. Please enter a different ID.") 
@@ -75,23 +104,20 @@ while True:
         scanned_ids.append(scanned_id)
 
 #HEADING
-print("---------------")
+dashed_line()
 print("Gregarious Groceries")
 print("915 7th Ave, New York City, NY 10019")
-print("---------------")
+
+
+dashed_line()
+print("Purchased: " + human_friendly_timestamp(now))
+
 
 #TIME AND DATE
-if int(current_hour) > 12 and int(current_minute) > 9:
-    print("Purchased at " + str(current_hour - 12) + ":" + str(current_minute) + "pm on " + str(current_month) + "/" + str(current_day) + "/" + str(current_year))
-elif int(current_hour) > 12 and int(current_minute) < 9:
-    print("Purchased at " + str(current_hour - 12) + ":0" + str(current_minute) + "pm on " + str(current_month) + "/" + str(current_day) + "/" + str(current_year))
-elif int(current_hour) < 12 and int(current_minute) < 9:
-    print("Purchased at " + str(current_hour) + ":0" + str(current_minute) + "am on " + str(current_month) + "/" + str(current_day) + "/" + str(current_year))
-else:
-    print("Purchased at " + str(current_time) + "am on " + str(current_month) + "/" + str(current_day) + "/" + str(current_year))
+now = datetime.now()
 
 #PURCHASED ITEMS
-print("---------------")
+dashed_line()
 print("Today you purchased: ")
 
 for scanned_id in scanned_ids:
@@ -104,29 +130,17 @@ for scanned_id in scanned_ids:
 tax_amt = .0875 * float(subtotal_price)
 total_price = subtotal_price + tax_amt
 
-print("---------------")
+dashed_line()
 print(f"Subtotal: {to_usd(subtotal_price)}")
 print(f"Tax: {to_usd(tax_amt)}")
 print(f"Total amount: {to_usd(total_price)}")
 
 #Thank you and come back
-print("---------------")
+dashed_line()
 print("Have a GREGARIOUS day and we hope to see you again soon!")
 print("Check us out at www.gregariousgroceries.com or give us a call at 202-954-3232")
-print("---------------")
+dashed_line()
 
-
-#Basic Requirements
-# A grocery store name of your choice 
-# A grocery store phone number and/or website URL and/or address of choice 
-# The date and time of the beginning of the checkout process, formatted in a human-friendly way (e.g. 2020-02-07 03:54 PM)
-# The name and price of each shopping cart item, price being formatted as US dollars and cents (e.g. $3.50, etc.)
-# The total cost of all shopping cart items (i.e. the "subtotal"), formatted as US dollars and cents (e.g. $19.47), calculated as the sum of their prices
-# The amount of tax owed (e.g. $1.70), calculated by multiplying the total cost by a New York City sales tax rate of 8.75% (for the purposes of this project, groceries are not exempt from sales tax)
-# The total amount owed, formatted as US dollars and cents (e.g. $21.17), calculated by adding together the amount of tax owed plus the total cost of all shopping cart items
-# A friendly message thanking the customer and/or encouraging the customer to shop again
-
-# shopping_cart.py
 
 #possible email attachment for future
 #er = input("Would you like an email copy of your recipet [y/n] ").lower()
